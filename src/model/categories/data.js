637d0a02c75ec3eded775510c845208ac2862257
mbio Peter <mbiopeter401@gmail.com>
2025-03-10 17:58:11 +0300

    separated categories data

---
+++ src/model/categories/data.js
@@ -0,0 +1,25 @@
+export const categories = [
+    {
+        name: "Woman’s Fashion",
+        link: "/womens-fashion",
+        subcategories: [
+            { name: "Dresses", link: "/womens-fashion/dresses" },
+            { name: "Tops", link: "/womens-fashion/tops" }
+        ]
+    },
+    {
+        name: "Men’s Fashion",
+        link: "/mens-fashion",
+        subcategories: [
+            { name: "Shirts", link: "/mens-fashion/shirts" },
+            { name: "Pants", link: "/mens-fashion/pants" }
+        ]
+    },
+    { name: "Electronics", link: "/electronics" },
+    { name: "Home & Lifestyle", link: "/home-lifestyle" },
+    { name: "Medicine", link: "/medicine" },
+    { name: "Sports & Outdoor", link: "/sports-outdoor" },
+    { name: "Baby’s & Toys", link: "/babys-toys" },
+    { name: "Groceries & Pets", link: "/groceries-pets" },
+    { name: "Health & Beauty", link: "/health-beauty" }
+];
--- src/views/components/categories/Categories.jsx
+++ src/views/components/categories/Categories.jsx
@@ -1,32 +1,8 @@
 import React, { useState } from "react";
 import { ExpandMore, ChevronRight } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import { categories } from "../../../model/categories/data";
 
-const categories = [
-    {
-        name: "Woman’s Fashion",
-        link: "/womens-fashion",
-        subcategories: [
-            { name: "Dresses", link: "/womens-fashion/dresses" },
-            { name: "Tops", link: "/womens-fashion/tops" }
-        ]
-    },
-    {
-        name: "Men’s Fashion",
-        link: "/mens-fashion",
-        subcategories: [
-            { name: "Shirts", link: "/mens-fashion/shirts" },
-            { name: "Pants", link: "/mens-fashion/pants" }
-        ]
-    },
-    { name: "Electronics", link: "/electronics" },
-    { name: "Home & Lifestyle", link: "/home-lifestyle" },
-    { name: "Medicine", link: "/medicine" },
-    { name: "Sports & Outdoor", link: "/sports-outdoor" },
-    { name: "Baby’s & Toys", link: "/babys-toys" },
-    { name: "Groceries & Pets", link: "/groceries-pets" },
-    { name: "Health & Beauty", link: "/health-beauty" }
-];
 
 const Categories = () => {
     const [openCategories, setOpenCategories] = useState({});
