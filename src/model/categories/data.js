1cddf2afb1af67b0372e5d4942474d576ac7829e
mbio Peter <mbiopeter401@gmail.com>
2025-03-10 23:23:01 +0300

    other categories

--- src/model/categories/data.js
+++ src/model/categories/data.js
@@ -1,3 +1,11 @@
+import phone from "../../assets/images/phone.png";
+import camera from "../../assets/images/camera.png";
+import computer from "../../assets/images/computer.png";
+import gamming from "../../assets/images/gaming.png";
+import headphone from "../../assets/images/headphone.png";
+import watch from "../../assets/images/watch.png";
+
+
 export const categories = [
     {
         name: "Woman’s Fashion",
@@ -19,3 +27,12 @@ export const categories = [
     { name: "Home & Lifestyle", link: "/home-lifestyle" },
     { name: "Medicine", link: "/medicine" },
 ];
+
+export const moreCategories = [
+    { name: "Phones", link: '/phones', img: phone },
+    { name: "Computers", link: '/computers', img: computer },
+    { name: "Smart Watch", link: '/smartwatch', img: watch },
+    { name: "Headphones", link: '/headphones', img: headphone },
+    { name: "Gamming", link: '/gamming', img: gamming },
+    { name: "Camera", link: '/camera', img: camera },
+]
--- src/views/pages/home/Home.jsx
+++ src/views/pages/home/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Categories from '../../components/categories/Categories'
-import Slider from '../../components/slider/Slider'
+import React, { useState } from 'react'
+import Categories from '../../components/categories/Categories';
+import Slider from '../../components/slider/Slider';
+import BlockCategory from '../../components/categories/block';
+import { moreCategories } from '../../../model/categories/data';
 
 const Home = () => {
+    const [activeCategory, setActiveCategory] = useState('Camera');
     return (
         <div className=' w-full flex  flex-col py-[20px] px-[50px] lg:px-[130px]'>
             <div className='flex flex-col gap-[20px] lg:flex-row justify-between'>
                 <Categories />
                 <Slider />
+            </div>
+            <div className='flex flex-row overflow-x-auto pb-[10px] lg:pb-[20px] gap-5 lg:gap-10 pt-15 border-t-[1px] border-t-[#ddd]'>
+                {moreCategories.map((item,index) => (
+                    <BlockCategory values={item} key={index} activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+                ))}
             </div>
         </div>
     )
